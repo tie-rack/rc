bce248daa1bd904d4dcd3de985adfd7c9896ace4
Chris Shea <cmshea@gmail.com>
2013-12-06 15:23:57 -0500

    Add .lein/profiles.clj

---
+++ lein/profiles.clj
@@ -0,0 +1,2 @@
+{:user {:plugins [[lein-kibit "0.0.8"]
+                  [jonase/eastwood "0.0.2"]]}}
--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ ln -nfs `pwd`/gitconfig ~/.gitconfig
 ln -nfs `pwd`/gitignore_global ~/.gitignore_global
 ln -nfs `pwd`/irbrc ~/.irbrc
 ln -nfs `pwd`/zshrc ~/.zshrc
+
+mkdir -p ~/.lein
+ln -nfs `pwd`/lein/profiles.clj ~/.lein/profiles.clj
