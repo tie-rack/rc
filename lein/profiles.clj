52460435bdc27c871a763a89beab4a9a485995f9
Chris Shea <cmshea@gmail.com>
2013-12-30 17:09:28 -0500

    A bunch of lein plugins

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,2 +1,7 @@
 {:user {:plugins [[lein-kibit "0.0.8"]
-                  [jonase/eastwood "0.0.2"]]}}
+                  [jonase/eastwood "0.0.2"]
+                  [lein-pprint "1.1.1"]
+                  [com.jakemccrary/lein-test-refresh "0.2.0"]
+                  [lein-cloverage "1.0.2"]
+                  [lein-marginalia "0.7.1"]
+                  [lein-try "0.4.1"]]}}
