e618e587834440cb6fe7b014f8eafc01cc37507e
Chris Shea <cmshea@gmail.com>
2014-02-03 11:47:19 -0500

    pedestal-datomic template

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,9 @@
-{:user {:plugins [[lein-kibit "0.0.8"]
-                  [jonase/eastwood "0.0.2"]
-                  [lein-pprint "1.1.1"]
-                  [com.jakemccrary/lein-test-refresh "0.2.0"]
-                  [lein-cloverage "1.0.2"]
-                  [lein-marginalia "0.7.1"]
-                  [lein-try "0.4.1"]]}}
+{:user
+ {:plugins [[pedestal-datomic/lein-template "0.1.0-SNAPSHOT"]
+            [lein-kibit "0.0.8"]
+            [jonase/eastwood "0.0.2"]
+            [lein-pprint "1.1.1"]
+            [com.jakemccrary/lein-test-refresh "0.2.0"]
+            [lein-cloverage "1.0.2"]
+            [lein-marginalia "0.7.1"]
+            [lein-try "0.4.1"]]}}
