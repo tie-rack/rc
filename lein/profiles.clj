01b6e4ac2aa64493a99f01c29f05c2b05f319d6c
Chris Shea <cmshea@gmail.com>
2014-03-14 11:53:11 -0400

    tools.nrepl, slamhound, kibit, lein-deps-tree

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,9 +1,11 @@
 {:user
- {:plugins [[pedestal-datomic/lein-template "0.1.0-SNAPSHOT"]
-            [lein-kibit "0.0.8"]
+ {:dependencies [[org.clojure/tools.nrepl "0.2.3"]
+                 [slamhound "1.5.1"]]
+  :plugins [[lein-kibit "0.0.8"]
             [jonase/eastwood "0.0.2"]
             [lein-pprint "1.1.1"]
             [com.jakemccrary/lein-test-refresh "0.2.0"]
             [lein-cloverage "1.0.2"]
+            [lein-deps-tree "0.1.2"]
             [lein-marginalia "0.7.1"]
             [lein-try "0.4.1"]]}}
