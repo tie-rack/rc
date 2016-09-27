dfea4fc5dd34b67919e1ebb4928bf31a679a9ec3
Christopher Shea <cmshea@gmail.com>
2016-09-27 13:22:11 -0400

    Cleanup lein profiles.clj

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,13 +1,5 @@
 {:user
- {:dependencies [[org.clojure/tools.nrepl "0.2.12"]
-                 [slamhound "1.5.1"]]
-  :plugins [[cider/cider-nrepl "0.10.0"]
-            [lein-kibit "0.0.8"]
-            [jonase/eastwood "0.0.2"]
-            [lein-pprint "1.1.1"]
-            [com.jakemccrary/lein-test-refresh "0.2.0"]
-            [lein-cloverage "1.0.2"]
-            ;[lein-deps-tree "0.1.2"]
-            [lein-immutant "1.2.0"]
-            [lein-marginalia "0.7.1"]
+ {:plugins [[lein-kibit "0.1.2"]
+            [jonase/eastwood "0.2.3"]
+            [lein-marginalia "0.9.0"]
             [lein-try "0.4.3"]]}}
