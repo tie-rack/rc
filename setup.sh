6bcf16c36c297c1068b28d522731800256f84046
Christopher Shea <cmshea@gmail.com>
2016-11-05 18:05:43 -0400

    Add tmux conf

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ ln -nfs `pwd`/gitconfig ~/.gitconfig
 ln -nfs `pwd`/gitignore_global ~/.gitignore_global
 ln -nfs `pwd`/irbrc ~/.irbrc
 ln -nfs `pwd`/zshrc ~/.zshrc
+ln -nfs `pwd`/tmux.conf ~/.tmux.conf
 
 mkdir -p ~/.lein
 ln -nfs `pwd`/lein/profiles.clj ~/.lein/profiles.clj
