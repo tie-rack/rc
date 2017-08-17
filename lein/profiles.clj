f51bf33eb9ed566bce21f3b7d0d3b0ec6cf896ef
Christopher Shea <cmshea@gmail.com>
2017-08-17 15:59:40 -0400

    lein ancient

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,4 +2,5 @@
  {:plugins [[lein-kibit "0.1.2"]
             [jonase/eastwood "0.2.3"]
             [lein-marginalia "0.9.0"]
-            [lein-try "0.4.3"]]}}
+            [lein-try "0.4.3"]
+            [lein-ancient "0.6.10"]]}}
